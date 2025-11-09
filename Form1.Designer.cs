48f9738d76bcebfae2494f7840aa3f8045a10c26
zzang <zzangism@gmail.com>
2025-11-08 23:11:32 -0500

    initialize

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,152 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnIfResult = new System.Windows.Forms.Button();
-            this.lblIfResult = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnSwitchResult = new System.Windows.Forms.Button();
-            this.nNumber1 = new System.Windows.Forms.NumericUpDown();
-            this.nNumber2 = new System.Windows.Forms.NumericUpDown();
-            this.cboxDay = new System.Windows.Forms.ComboBox();
-            this.lblSwitchResult = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.nNumber1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nNumber2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Number1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(187, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Number2";
-            // 
-            // btnIfResult
-            // 
-            this.btnIfResult.Location = new System.Drawing.Point(342, 44);
-            this.btnIfResult.Name = "btnIfResult";
-            this.btnIfResult.Size = new System.Drawing.Size(133, 64);
-            this.btnIfResult.TabIndex = 2;
-            this.btnIfResult.Text = "Compare Number";
-            this.btnIfResult.UseVisualStyleBackColor = true;
-            this.btnIfResult.Click += new System.EventHandler(this.btnIfResult_Click);
-            // 
-            // lblIfResult
-            // 
-            this.lblIfResult.AutoSize = true;
-            this.lblIfResult.Location = new System.Drawing.Point(43, 132);
-            this.lblIfResult.Name = "lblIfResult";
-            this.lblIfResult.Size = new System.Drawing.Size(17, 18);
-            this.lblIfResult.TabIndex = 3;
-            this.lblIfResult.Text = "-";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 221);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 18);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Day";
-            // 
-            // btnSwitchResult
-            // 
-            this.btnSwitchResult.Location = new System.Drawing.Point(342, 230);
-            this.btnSwitchResult.Name = "btnSwitchResult";
-            this.btnSwitchResult.Size = new System.Drawing.Size(133, 64);
-            this.btnSwitchResult.TabIndex = 6;
-            this.btnSwitchResult.Text = "Selected Day";
-            this.btnSwitchResult.UseVisualStyleBackColor = true;
-            this.btnSwitchResult.Click += new System.EventHandler(this.btnSwitchResult_Click);
-            // 
-            // nNumber1
-            // 
-            this.nNumber1.Location = new System.Drawing.Point(43, 65);
-            this.nNumber1.Name = "nNumber1";
-            this.nNumber1.Size = new System.Drawing.Size(120, 28);
-            this.nNumber1.TabIndex = 7;
-            // 
-            // nNumber2
-            // 
-            this.nNumber2.Location = new System.Drawing.Point(190, 65);
-            this.nNumber2.Name = "nNumber2";
-            this.nNumber2.Size = new System.Drawing.Size(120, 28);
-            this.nNumber2.TabIndex = 8;
-            // 
-            // cboxDay
-            // 
-            this.cboxDay.FormattingEnabled = true;
-            this.cboxDay.Items.AddRange(new object[] {
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday"});
-            this.cboxDay.Location = new System.Drawing.Point(46, 250);
-            this.cboxDay.Name = "cboxDay";
-            this.cboxDay.Size = new System.Drawing.Size(121, 26);
-            this.cboxDay.TabIndex = 9;
-            // 
-            // lblSwitchResult
-            // 
-            this.lblSwitchResult.AutoSize = true;
-            this.lblSwitchResult.Location = new System.Drawing.Point(55, 324);
-            this.lblSwitchResult.Name = "lblSwitchResult";
-            this.lblSwitchResult.Size = new System.Drawing.Size(17, 18);
-            this.lblSwitchResult.TabIndex = 10;
-            this.lblSwitchResult.Text = "-";
             // 
             // form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 429);
-            this.Controls.Add(this.lblSwitchResult);
-            this.Controls.Add(this.cboxDay);
-            this.Controls.Add(this.nNumber2);
-            this.Controls.Add(this.nNumber1);
-            this.Controls.Add(this.btnSwitchResult);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.lblIfResult);
-            this.Controls.Add(this.btnIfResult);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(368, 286);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.nNumber1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nNumber2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnIfResult;
-        private System.Windows.Forms.Label lblIfResult;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnSwitchResult;
-        private System.Windows.Forms.NumericUpDown nNumber1;
-        private System.Windows.Forms.NumericUpDown nNumber2;
-        private System.Windows.Forms.ComboBox cboxDay;
-        private System.Windows.Forms.Label lblSwitchResult;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,8 +15,7 @@ namespace Practice
         public form1()
         {
             InitializeComponent();
-            ifTest();
-            switchTest();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -24,99 +23,5 @@ namespace Practice
 
         }
 
-        private void switchTest()
-        {
-            int iRet = 3;
-            string strResult = "";
-            switch (iRet)
-            {
-                case 3:
-                    {
-                        strResult = "2 or 3";
-                    }
-                    break;
-                case 4:
-                    {
-                        strResult = "4";
-                    }
-                    break;
-                default:
-                    {
-                        strResult = "I do not know";
-                    }
-                    break;
-            }
-        }
-
-
-        private void ifTest()
-        {
-            int ia = 30;
-            int ib = 20;
-
-            string strResult = "";
-
-            if (ia > ib)
-            {
-                strResult = "ia is bigger";
-            }
-            else if (ia < ib)
-            {
-                strResult = "ia is smaller";
-            }
-            else
-            {
-                strResult = "ia, ib are same";
-            }
-
-            strResult = (ia > ib) ? "ia is bigger" : "ia, ib are same";
-
-        }
-
-        private void btnIfResult_Click(object sender, EventArgs e)
-        {
-            int iNumber1 = (int)nNumber1.Value;
-            int iNumber2 = (int)nNumber2.Value;
-
-            if(iNumber1 > iNumber2)
-            {
-                lblIfResult.Text = string.Format("- Number1 is {0} bigger than Number2.", iNumber1 - iNumber2);
-            }
-            else if (iNumber1 < iNumber2)
-            {
-                lblIfResult.Text = string.Format("- Number2 is {0} bigger than Number1.", iNumber2 - iNumber1);
-            }
-            else
-            {
-                lblIfResult.Text =string.Format( "Number 1,2 are same. Number is {0}" , iNumber1);
-            }
-        }
-
-        private void btnSwitchResult_Click(object sender, EventArgs e)
-        {
-            string strSelect = cboxDay.Text;
-
-            switch (strSelect)
-            {
-                case "Monday":
-                    lblSwitchResult.Text = "- Selected day is Monday";
-                    break;
-                case "TuesDay":
-                    lblSwitchResult.Text = "- Selected day is TuesDay";
-                    break;
-                case "Wednesday":
-                    lblSwitchResult.Text = "- Selected day is Wednesday";
-                    break;
-                case "Thursday":
-                    lblSwitchResult.Text = "- Selected day is Thursday";
-                    break;
-                case "Friday":
-                    lblSwitchResult.Text = "- Selected day is Friday";
-                    break;
-                default:
-                    lblSwitchResult.Text = "- Selected day is Weekend";
-                    break;
-            }
-        }
     }
 }
